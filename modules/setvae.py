f2837bc5de82da01c4747c0c47e60d56b04eb044
Warspoot <106011484+Warspoot@users.noreply.github.com>
2023-04-23 15:15:09 +0930

    fix setvae errors

--- modules/setvae.py
+++ modules/setvae.py
@@ -12,8 +12,9 @@ from nextcord.ext import commands
 from PIL import Image, PngImagePlugin
 from app import *
 
-vae_if = "nai.vae.pt" or "orangemix.vae.pt" or "none" or "Automatic"
+vae_if = ["nai.vae.pt", "orangemix.vae.pt", "none", "Automatic"]
 vae_list = "`nai.vae.pt` or `orangemix.vae.pt` or `none` or `Automatic`"
+
 class setvae(commands.Cog):
     def __init__ (self, bot):
         self.bot = bot
@@ -27,12 +28,14 @@ class setvae(commands.Cog):
         self,
         interaction : Interaction,
         vae : str,
-        ):
-        if vae != vae_if:
+    ):
+        if vae in vae_if:
+            vae = vae
+        else:
             vae = "Automatic"
-            await interaction.response.send_message("Invalid VAE! Setting VAE to Automatic")
-
-        await interaction.channel.send("Just a moment...")
+            await interaction.channel.send(f"Invalid VAE! Setting VAE to Automatic. Valid options are: {vae_list}")
+            
+        await interaction.response.send_message("Just a moment...")
         global option_payload
         option_payload = {
             "sd_vae" : (vae),
@@ -42,6 +45,7 @@ class setvae(commands.Cog):
 
         global response
         response = requests.post(url=f'{url}/sdapi/v1/options', json = option_payload)
-        await interaction.followup.send(f"VAE has been set (if invalid VAE was given then VAE is set to Audomatic)")
+        await interaction.channel.send(f"VAE has been set (if invalid VAE was given then VAE is set to Automatic)")
+
 def setup(bot):
     bot.add_cog(setvae(bot))
